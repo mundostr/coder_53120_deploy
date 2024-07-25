27bd4e9a8e7763a450a9ebd26d01d6b0d87b036c
Carlos Perren <idux.net@gmail.com>
2024-07-24 21:24:30 -0300

    Commit para deploy

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+
+import usersRouter from './routes/users.router.js';
+import petsRouter from './routes/pets.router.js';
+import adoptionsRouter from './routes/adoption.router.js';
+import sessionsRouter from './routes/sessions.router.js';
+
+import { engine } from 'express-handlebars';
+import {fileURLToPath} from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+// const connection = mongoose.connect(process.env.MONGO_URL);
+const connection = mongoose.connect('mongodb://127.0.0.1:27017/coder_53120');
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users',usersRouter);
+app.use('/api/pets',petsRouter);
+app.use('/api/adoptions',adoptionsRouter);
+app.use('/api/sessions',sessionsRouter);
+
+app.all('*', (req, res) => { res.status(404).send('No se encuentra el endpoint solicitado') })
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/views`);
+
+app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
